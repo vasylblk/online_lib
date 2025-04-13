5f95f6293bae4e809d8f5c1e24aa60f32ef128d6
vasylblk <bulak.vasyl@lll.kpi.ua>
2025-04-13 23:37:32 +0300

    first attempt

---
+++ reading-service/dist/reading-progress/dto/create-reading-progress.dto.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateReadingProgressDto = void 0;
+class CreateReadingProgressDto {
+}
+exports.CreateReadingProgressDto = CreateReadingProgressDto;
